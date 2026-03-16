12bc315b63407f400e4778ad2d3e746bde1ef30d
DaverDavid <daverdavids@gmail.com>
2026-03-16 10:45:04 -0400

    binary

--- SF-ESP32-Clients/SF-ESP32-Clients.ino
+++ SF-ESP32-Clients/SF-ESP32-Clients.ino
@@ -13,7 +13,7 @@
 #include <stdarg.h>
 #include <esp_system.h>
 
-#define DEBUG_SERIAL 0
+#define DEBUG_SERIAL 1
 #if DEBUG_SERIAL
   #define DBG_BEGIN(x) Serial.begin(x)
   #define DBG(...)     Serial.printf(__VA_ARGS__)
